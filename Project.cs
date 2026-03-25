02eece7b875fca7d51d9de06b3075df88cbb5efd
Purple-Towel <angadgrewal31@gmail.com>
2026-03-24 21:57:38 -0700

    Initial draft of the Project DTO

---
+++ Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP_3951_BlockForge_TechPro
+{
+    public class Project
+    {
+        public int Version { get; set; } = 1;
+        public string? ProjectName { get; set; }
+        public List<CodeBlock>? CodeBlocks { get; set; } = new List<CodeBlock>();
+    }
+}
