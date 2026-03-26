488105ed0c23ccfdedf35ad6ea2dc5955801e024
adilascio <adilascio@my.bcit.ca>
2026-03-25 17:33:18 -0700

    fixed overlapping blocks in the grid, console now sends message when a block is created and when a block is placed onto an already occupied cell in the grid

--- Form1.cs
+++ Form1.cs
@@ -188,6 +188,13 @@ namespace COMP_3951_BlockForge_TechPro
             dropPoint.Offset(-newBlock.Width / 2, -newBlock.Height / 2);
 
             SnappedPlacement snappedPlacement = _gridSnapService.Snap(dropPoint, newBlock.Size, WorkspaceBounds);
+
+            if (IsGridCellOccupied(snappedPlacement.GridPosition))
+            {
+                AppendConsoleMessage(ConsoleMessageSeverity.Warning, "That grid cell is already occupied.");
+                return;
+            }
+
             newBlock.Location = snappedPlacement.Location;
             groupBoxWorkSpace.Controls.Add(newBlock);
             newBlock.BringToFront();
@@ -237,7 +244,7 @@ namespace COMP_3951_BlockForge_TechPro
         // Drag blocks around inside WorkSpace 
         private bool _dragging = false;
         private Point _dragOffset; // mouse offset within the panel being dragged
-        private Panel _activeBlock;
+        private Panel? _activeBlock;
 
         private void WorkspaceBlock_MouseDown(object sender, MouseEventArgs e)
         {
@@ -271,8 +278,17 @@ namespace COMP_3951_BlockForge_TechPro
             if (_activeBlock != null)
             {
                 SnappedPlacement snappedPlacement = _gridSnapService.Snap(_activeBlock.Location, _activeBlock.Size, WorkspaceBounds);
-                _activeBlock.Location = snappedPlacement.Location;
-                UpdateStoredBlockPosition(_activeBlock, snappedPlacement);
+
+                if (IsGridCellOccupied(snappedPlacement.GridPosition, _activeBlock))
+                {
+                    AppendConsoleMessage(ConsoleMessageSeverity.Warning, "That grid cell is already occupied.");
+                    RestoreStoredBlockPosition(_activeBlock);
+                }
+                else
+                {
+                    _activeBlock.Location = snappedPlacement.Location;
+                    UpdateStoredBlockPosition(_activeBlock, snappedPlacement);
+                }
             }
 
             _dragging = false;
@@ -291,6 +307,7 @@ namespace COMP_3951_BlockForge_TechPro
                 snappedPlacement.GridPosition.Row);
 
             _workspaceBlocks[blockPanel] = codeBlock;
+            AppendConsoleMessage(ConsoleMessageSeverity.Message, $"{blockName} block created.");
         }
 
         private void UpdateStoredBlockPosition(Panel blockPanel, SnappedPlacement snappedPlacement)
@@ -302,6 +319,35 @@ namespace COMP_3951_BlockForge_TechPro
 
             codeBlock.UpdatePosition(snappedPlacement.Location.X, snappedPlacement.Location.Y);
             codeBlock.UpdateGridPosition(snappedPlacement.GridPosition.Column, snappedPlacement.GridPosition.Row);
+        }
+
+        private void RestoreStoredBlockPosition(Panel blockPanel)
+        {
+            if (!_workspaceBlocks.TryGetValue(blockPanel, out CodeBlock? codeBlock))
+            {
+                return;
+            }
+
+            blockPanel.Location = new Point((int)codeBlock.PosX, (int)codeBlock.PosY);
+        }
+
+        private bool IsGridCellOccupied(GridPosition gridPosition, Panel? movingBlock = null)
+        {
+            foreach (KeyValuePair<Panel, CodeBlock> workspaceBlock in _workspaceBlocks)
+            {
+                if (workspaceBlock.Key == movingBlock)
+                {
+                    continue;
+                }
+
+                if (workspaceBlock.Value.GridColumn == gridPosition.Column &&
+                    workspaceBlock.Value.GridRow == gridPosition.Row)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         // keep blocks inside workspace bounds
