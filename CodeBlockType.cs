a241d0ad047f2ce89a2f6e5c4e18a1718824e3fd
Purple-Towel <angadgrewal31@gmail.com>
2026-04-07 20:34:52 -0700

    Included updates to CodeBlockType.cs to support new functionality.

--- CodeBlockType.cs
+++ CodeBlockType.cs
@@ -3,7 +3,11 @@ namespace COMP_3951_BlockForge_TechPro
     public enum CodeBlockType
     {
         Unknown,
+        Start,
+        End,
         If,
+        Then,
+        Else,
         While,
         Run,
         Print,
